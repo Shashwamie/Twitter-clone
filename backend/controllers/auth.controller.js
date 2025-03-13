7f91e81440a739cd6decfcb13ea3fba62aaf4256
Shashwamie <shashwamie@gmail.com>
2025-03-12 21:05:14 -0600

    1- project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (res, req) => {
+    res.json({
+        data: "You hit the signup endpoint"
+    })
+}
+
+export const login = async (res, req) => {
+    res.json({
+        data: "You hit the login endpoint"
+    })
+}
+
+export const logout = async (res, req) => {
+    res.json({
+        data: "You hit the logout endpoint"
+    })
+}
