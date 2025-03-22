90c82e4cfc73fb0fd01e460d04f8d8feae7beb4c
Shashwamie <shashwamie@gmail.com>
2025-03-21 19:24:35 -0600

    1- post routes and controllers added

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { createPost, deletePost, likeUnlikePost, commentOnPost, getAllPosts, getLikedPosts, getFollowingPosts, getUserPosts } from '../controllers/post.controller.js'
+
+const router = express.Router();
+
+router.get("/all", protectRoute,getAllPosts)
+router.get("/following", protectRoute, getFollowingPosts)
+router.get("/likes/:id", protectRoute, getLikedPosts)
+router.get("/user/:username", protectRoute, getUserPosts)
+router.post("/create", protectRoute, createPost);
+router.post("/:id", protectRoute, deletePost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comments/:id", protectRoute, commentOnPost);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from 'cloudinary'
+
+// Routes
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
+import postRoutes from "./routes/post.routes.js"
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -25,7 +28,8 @@ app.use(express.urlencoded({ extended: true })); // to parse form data(url encod
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
